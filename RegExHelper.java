e2ea4b2a5b83bad37a2090780f18bc9e723bc7b4
ametsch <22308539+ametsch@users.noreply.github.com>
2021-02-11 14:10:04 -0500

    Add files via upload

--- RegExHelper.java
+++ RegExHelper.java
@@ -7,7 +7,7 @@ public class RegExHelper {
     public static final Pattern isAlpha = Pattern.compile("^[a-zA-Z]*$");
     public static final Pattern isNumeric = Pattern.compile("^[0-9]*$");
     public static final Pattern isHex = Pattern.compile("^[0-9a-fA-F]*$");
-    public static final Pattern isDecimal = Pattern.compile("^[0-9]+\\.[0-9]*?$");
+    public static final Pattern isFloat = Pattern.compile("^[0-9]+\\.[0-9]*?$");
 
     public static final Pattern isAlphaNumericSpace = Pattern.compile("^[ a-zA-Z0-9]*$");
     public static final Pattern isAlphaSpace = Pattern.compile("^[ a-zA-Z]*$");
@@ -21,6 +21,12 @@ public class RegExHelper {
     public static final Pattern isURL = Pattern.compile("^(https?|ftps?|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$");
 
     public static final Pattern isUnixTimestamp = Pattern.compile("^[0-9]+$");
+
+    public static final Pattern isBinary = Pattern.compile("^[0-1]+$");
+    public static final Pattern isOctal = Pattern.compile("^[0-7]+$");
+    public static final Pattern isRomanNumeral = Pattern.compile("^[IiVvXxLlCcDdMm]+$");
+    public static final Pattern isYTBase64 = Pattern.compile("^[0-9a-zA-Z-_]+$");
+
 
 
     // Date formats based on this list: https://docs.oracle.com/cd/E41183_01/DR/Date_Format_Types.html
