1cbb992e6fbeb73b6cc173f436cc294154cfffa5
ametsch <22308539+ametsch@users.noreply.github.com>
2021-02-11 14:05:24 -0500

    Add files via upload

--- RegExHelper.java
+++ RegExHelper.java
@@ -20,6 +20,18 @@ public class RegExHelper {
 
     public static final Pattern isURL = Pattern.compile("^(https?|ftps?|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$");
 
+    public static final Pattern isUnixTimestamp = Pattern.compile("^[0-9]+$");
+
+
+    // Date formats based on this list: https://docs.oracle.com/cd/E41183_01/DR/Date_Format_Types.html
+    public static final Pattern isDate1 = Pattern.compile("^[0-1][0-9]/[0-3][0-9]/[0-9]{4}$");
+    public static final Pattern isDate2 = Pattern.compile("^[0-3][0-9]/[0-1][0-9]/[0-9]{4}$");
+    public static final Pattern isDate3 = Pattern.compile("^[0-9]{4}/[0-1][0-9]/[0-3][0-9]$");
+    public static final Pattern isDateB = Pattern.compile("^[0-1][0-9][0-3][0-9][0-9]{4}$");
+    public static final Pattern isDateC = Pattern.compile("^[0-3][0-9][0-1][0-9][0-9]{4}$");
+    public static final Pattern isDateD = Pattern.compile("^[0-9]{4}[0-1][0-9][0-3][0-9]$");
+
+
     public static boolean match(Pattern pat, String str){
         Matcher m = pat.matcher(str);
         return m.matches();
