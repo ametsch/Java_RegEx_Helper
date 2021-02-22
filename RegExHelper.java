212a4cde35db768149043d63ef3cf3e31dbe4fe0
ametsch <22308539+ametsch@users.noreply.github.com>
2021-02-22 09:24:06 -0500

    Add files via upload

--- RegExHelper.java
+++ RegExHelper.java
@@ -3,18 +3,18 @@ package Ch10ArrayLists;
 import java.util.regex.*;
 
 public class RegExHelper {
-    public static final Pattern isAlphaNumeric = Pattern.compile("^[a-zA-Z0-9]*$");
-    public static final Pattern isAlpha = Pattern.compile("^[a-zA-Z]*$");
-    public static final Pattern isNumeric = Pattern.compile("^[0-9]*$");
-    public static final Pattern isHex = Pattern.compile("^[0-9a-fA-F]*$");
+    public static final Pattern isAlphaNumeric = Pattern.compile("^[a-zA-Z0-9]+$");
+    public static final Pattern isAlpha = Pattern.compile("^[a-zA-Z]+$");
+    public static final Pattern isNumeric = Pattern.compile("^[0-9]+$");
+    public static final Pattern isHex = Pattern.compile("^[0-9a-fA-F]+$");
     public static final Pattern isFloat = Pattern.compile("^[0-9]+\\.[0-9]*?$");
 
-    public static final Pattern isAlphaNumericSpace = Pattern.compile("^[ a-zA-Z0-9]*$");
-    public static final Pattern isAlphaSpace = Pattern.compile("^[ a-zA-Z]*$");
-    public static final Pattern isNumericSpace = Pattern.compile("^[ 0-9]*$");
-    public static final Pattern isHexSpace = Pattern.compile("^[ 0-9a-fA-F]*$");
+    public static final Pattern isAlphaNumericSpace = Pattern.compile("^[ a-zA-Z0-9]+$");
+    public static final Pattern isAlphaSpace = Pattern.compile("^[ a-zA-Z]+$");
+    public static final Pattern isNumericSpace = Pattern.compile("^[ 0-9]+$");
+    public static final Pattern isHexSpace = Pattern.compile("^[ 0-9a-fA-F]+$");
 
-    public static final Pattern isASCII = Pattern.compile("^[\\x00-\\x7F]*$");
+    public static final Pattern isASCII = Pattern.compile("^[\\x00-\\x7F]+$");
 
     public static final Pattern isEmail = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
 
@@ -26,6 +26,9 @@ public class RegExHelper {
     public static final Pattern isOctal = Pattern.compile("^[0-7]+$");
     public static final Pattern isRomanNumeral = Pattern.compile("^[IiVvXxLlCcDdMm]+$");
     public static final Pattern isYTBase64 = Pattern.compile("^[0-9a-zA-Z-_]+$");
+
+    public static final Pattern isByteBinary = Pattern.compile("^[0-1]{8}$");
+    public static final Pattern isByteHex = Pattern.compile("^[0-9a-fA-F]{2}$");
 
 
 
